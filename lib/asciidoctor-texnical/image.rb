796bd31df4f0b34f29f60b793752a9a2286ab05a
Holger Peters <email@holger-peters.de>
2018-12-22 23:18:45 +0100

    Parse image dimensions

--- lib/asciidoctor-texnical/image.rb
+++ lib/asciidoctor-texnical/image.rb
@@ -33,19 +33,25 @@ class Image
     target_path = File.absolute_path handler.image_target_dir
     dir = Dir.mktmpdir
     tpth = File.join dir, img_filename
+    stdout = ''
     Dir.chdir(dir) do
       File.open('eq.tex', 'w') { |file| file.write(content) }
       Open3.popen3('latex -interaction=batchmode eq.tex') do |_stdin, _stdout, _stderr, wait_thr|
         raise ValueError if wait_thr.value != 0
       end
-      Open3.popen3("dvipng -o #{tpth} -T tight -z9 -D 400 eq.dvi") do |_stdin, _stdout, _stderr, wait_thr|
-        raise ValueError if wait_thr.value != 0
-      end
+      stdout, = Open3.capture2("dvipng -o #{tpth} -T tight -z9 -D 400 -q --height --width eq.dvi")
       FileUtils.cp(tpth, target_path)
     end
 
     img_target = ::File.join handler.image_target_dir, img_filename
+    width, height = extract_dimensions_from_output(stdout)
     Image.new input, width, height, img_target
+  end
+
+  def self.extract_dimensions_from_output(captured_stdout)
+    height = captured_stdout.scan(/height=-?(\d+)/)
+    width = captured_stdout.scan(/width=(\d+)/)
+    [width[0][0].to_i, height[0][0].to_i]
   end
 
   def initialize(input, width, height, img_target)
--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -12,8 +12,12 @@ describe Image do
       Dir.mkdir target_path
       mathematical = Mathematical.new 140, target_path
       img = Image.generate_block('x^2 + x', 'equid', mathematical)
-      expect(File.join(target_path, 'equid.png')).to eql(img.target_path)
       expect(File.exist?(File.join(target_path, 'equid.png'))).to be true
+
+      # test for attributes of image
+      expect(File.join(target_path, 'equid.png')).to eql(img.target_path)
+      expect(img.width).to be 177 # who knows if this is stable
+      expect(img.height).to be 389 # who knows if this is stable
     end
   end
 end
